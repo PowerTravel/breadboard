de377d3847d44f14a0286efeee228a1b85c08eaa
PowerTravel <jakob.hariz@gmail.com>
2022-05-18 10:45:25 +0200

    Add vcvarsall files

--- code/build_game.bat
+++ code/build_game.bat
@@ -1,5 +1,5 @@
 @echo off
 REM This script first sets the environment to compile w vc - vcvarsall x64
 REM then compiles. Used when compiling from the game
-call "w:/breadboard2/misc/shell.bat"
-call "w:/breadboard2/code/build.bat"
+call "../misc/vcvarsall.bat"
+call "../code/build.bat"
---
+++ misc/vcvarsall.bat
@@ -0,0 +1,7 @@
+@echo off
+
+REM This is for windows 7:
+REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+
+REM This is for windows 10:
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
